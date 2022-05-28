d38f304beb1de1eaa211881ee0fd86c23e2d08fa
Moaziz-me <moaziz.me@outlook.com>
2022-05-28 20:31:16 +1000

    working with shiny

---
+++ Statictable with KableExtra/ui.R
@@ -0,0 +1,24 @@
+library("kableExtra")
+
+navbarPage(
+  "shiny::renderTable",
+  tabPanel(
+    "Star Narrow",
+    fluidPage(
+      tableOutput("star_narrow")
+    )
+  ),
+  tabPanel(
+    "Star Wide",
+    fluidPage(
+      tableOutput("star_wide")
+    )
+  ),
+  tabPanel(
+    "Star List",
+    fluidPage(
+      tableOutput("star_lists")
+    )
+  ),
+  collapsible = TRUE
+)
